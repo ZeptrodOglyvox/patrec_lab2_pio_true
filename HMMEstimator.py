ef5ae7e1069e7b45ca2a5687cbff161873472c42
ZeptrodOglyvox <ntsakas42@gmail.com>
2019-12-30 02:11:07 +0200

    Initial Commit

---
+++ HMMEstimator.py
@@ -0,0 +1,96 @@
+from pomegranate import *
+import numpy as np
+
+# Dearest Panika,
+# Read Digit Model and then HMMEstimator, most of it is copied for their hmm.py script
+
+
+# This class trains an HMM model for one digit label, 10 of these will be made
+class DigitModel:
+    def __init__(self):
+        self.model = None
+
+    def fit(self, trans_mat, starts, X, data, n_states=2, n_mixtures=2, gmm=True, max_iterations=1000):
+        dists = []  # list of probability distributions for the HMM states
+        for i in range(n_states):
+            if gmm:
+                a = GeneralMixtureModel.from_samples(MultivariateGaussianDistribution, n_mixtures, X)
+            else:
+                a = MultivariateGaussianDistribution.from_samples(X)
+            dists.append(a)
+
+        model = HiddenMarkovModel.from_matrix(trans_mat, dists, starts, state_names=[f's{i}' for i in range(n_states)])
+        model.fit(data, max_iterations=max_iterations)
+
+        self.model = model
+
+    def predict(self, X_test):
+        logp, _ = self.model.viterbi(X_test)  # Run viterbi algorithm and return log-probability
+        return logp
+
+
+# Boring function to hide these lines of code, initializing as instructed
+def initialize_parameters(n_states):
+    trans_matrix = np.zeros((n_states, n_states))
+    for i in range(n_states):
+        e = 1e-5
+        p = np.random.uniform(0 + e, 1)  # Getting zero probabilities might not be desired
+        if i + 1 < n_states:
+            trans_matrix[i][i + 1] = p
+            trans_matrix[i][i] = 1 - p
+        else:
+            trans_matrix[i][i] = 1  # TODO: Is this right?
+
+    starts = np.zeros(n_states)
+    starts[0] = 1
+
+    return trans_matrix, starts
+
+
+# Wrapper class for the overall estimator
+class HMMEstimator:
+    def __init__(self, n_states, n_mixtures):
+        self.models = []    # Will contain the 10 models
+
+        # Hyperparameters
+        self.n_states = n_states
+        self.n_mixtures = n_mixtures
+
+    def fit(self, X_train=None, y_train=None):
+        for digit in range(10):
+            # Create Data Array
+            digit_train = X_train[y_train == digit]
+            X = digit_train[0]
+            for i in range(1, digit_train.shape[0]):
+                X = np.concatenate((X, X_train[i]), axis=0)
+
+            # Create and optimize parameters
+            trans_matrix, starts = initialize_parameters(self.n_states)
+
+            # Train model for this digit
+            model = DigitModel()
+            model.fit(trans_matrix, starts, X, list(digit_train), n_states=self.n_states, n_mixtures=self.n_mixtures)
+            self.models.append(model)
+
+    def predict(self, X_test):
+        y_pred = []
+        for sample in X_test:
+            likelihoods = []
+            for model in self.models:
+                likelihoods.append(
+                    model.predict(sample)
+                )
+
+            y_pred.append(
+                np.argmax(likelihoods)
+            )
+
+        return np.array(y_pred)
+
+    def score(self, X_test, y_test):
+        y_pred = self.predict(X_test)
+
+        return np.mean(y_test == y_pred)
+
+
+
---
+++ lab2_pt1.py
@@ -0,0 +1,107 @@
+import numpy as np
+from pr_lab2_2019_20_help_scripts.lab2_help.parser import parser
+from HMMEstimator import HMMEstimator
+import os
+from sklearn.model_selection import StratifiedShuffleSplit
+import csv
+
+read = False  # Change to re-read data
+data_dir = os.path.join(os.getcwd(), 'fsdd-recordings')
+
+# If this folder doesn't exist you might get an error, create it manually
+save_dir = os.path.join(os.getcwd(), 'pickles')
+
+
+# *** 1. Prepare Data ***
+if read:
+    data = parser(data_dir)
+    X_train_full, X_test, y_train_full, y_test, spk_train, spk_test = parser(data_dir)
+    np.save(arr=np.array(X_train_full), file=os.path.join(save_dir, 'X_train'))
+    np.save(arr=np.array(X_test), file=os.path.join(save_dir, 'X_test'))
+    np.save(arr=np.array(y_train_full), file=os.path.join(save_dir, 'y_train'))
+    np.save(arr=np.array(y_test), file=os.path.join(save_dir, 'y_test'))
+    np.save(arr=np.array(spk_train), file=os.path.join(save_dir, 'spk_train'))
+    np.save(arr=np.array(spk_test), file=os.path.join(save_dir, 'spk_test'))
+else:
+    X_train_full = np.load(os.path.join(save_dir, 'X_train.npy'), allow_pickle=True)
+    X_test = np.load(os.path.join(save_dir, 'X_test.npy'), allow_pickle=True)
+    y_train_full = np.load(os.path.join(save_dir, 'y_train.npy'), allow_pickle=True)
+    y_test = np.load(os.path.join(save_dir, 'y_test.npy'), allow_pickle=True)
+    spk_train = np.load(os.path.join(save_dir, 'spk_train.npy'), allow_pickle=True)
+    spk_test = np.load(os.path.join(save_dir, 'spk_test.npy'), allow_pickle=True)
+
+sss = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42069)
+train_idx, val_idx = sss.split(X_train_full, y_train_full).__next__()
+X_train, X_val = X_train_full[train_idx], X_train_full[val_idx]
+y_train, y_val = y_train_full[train_idx], y_train_full[val_idx]
+
+
+# *** 2. Set HMM Parameters ***
+# This code is actually useless here, it appears again as a function in the HMMEstimator module I made
+# Probably TODO: delete
+n_states = 5
+trans_matrix = np.zeros((n_states, n_states))
+for i in range(n_states):
+    e = 1e-5
+    p = np.random.uniform(0+e, 1)  # Getting zero probabilities might not be desired
+    if i + 1 < n_states:
+        trans_matrix[i][i+1] = p
+        trans_matrix[i][i] = 1-p
+    else:
+        trans_matrix[i][i] = 1
+
+starts = np.zeros(n_states)
+starts[0] = 1  # Or maybe they mean starts[1]? Does it matter?
+
+
+# *** 3. Train 10 GMM-HMM models ***
+best_n_states = 2
+best_n_mixtures = 2
+optimize_parameters = True  # True to do the grid-search again
+
+# Should we do a new stratified shuffle split for train and val sets every time?
+# Should we do k-fold cross-validation for each set of hypes?
+if optimize_parameters:
+    scores = np.zeros((4, 5))
+    for n_states in range(1, 5):
+        for n_mixtures in range(1, 6):
+            # Our own class, read it to get what's going on
+            gmm_hmm = HMMEstimator(n_states, n_mixtures)
+            HMMEstimator.fit(X_train, y_train)
+            score = HMMEstimator.score(X_val, y_val)
+            scores[n_states][n_mixtures] = score
+
+    # argmax() returns the index in the flattened array
+    idx = scores.argmax()
+
+    # np.unravel_index(flat_index, ndarray_shape): Best function ever, takes a flat index and
+    # returns the corresponding indices for an ndarray of given shape
+    best_n_states, best_n_mixtures = np.unravel_index(idx, scores.shape)
+
+    with open(os.path.join(os.getcwd(), 'best_hyperparameters.txt', 'wb')) as f:
+        f.write(f'{best_n_states}, {best_n_mixtures}\n')
+else:
+    r = csv.reader(open(os.path.join(os.getcwd(), 'best_hyperparameters.txt')))
+    best_n_states, best_n_mixtures = r.__next__()
+    best_n_states, best_n_mixtures = int(best_n_states), int(best_n_mixtures)
+
+
+# *** 4. Test the final model ***
+gmm_hmm = HMMEstimator(best_n_states, best_n_mixtures)
+gmm_hmm.fit(X_train_full, y_train_full)
+final_score = gmm_hmm.score(X_test, y_test)
+
+print(
+    f'My model brings all the boys to the yard\n'
+    f'and damn right, it\'s better that yours:\n'
+    f'Score: {final_score}\n'
+    f'damn right, it\'s better that yours\n'
+    f'Score: {final_score}'
+)
+
+
+def confusion_matrix(self, y_test, y_pred):
+    cf = np.zeros([y_pred.shape[0]] * 2)
+    for t, p in y_test, y_pred:
+        cf[t][p] += 1
+
